e4d3e6dc083961eaa0565e37d8e5c2b65e891c9c
Gaurav Singh <gs1293@gmail.com>
2019-05-06 19:23:36 +0530

    Updating config files

--- .config/i3/programs.sh
+++ .config/i3/programs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-choices="ranger\nvim\nchrome\nblueman\npavucontrol\nvimrc\nzshrc\ni3config"
+choices="ranger\nvim\nchrome\nblueman\npavucontrol\nvimrc\nzshrc\ni3config\nxdman\nqbittorrent\ncmus\ncava"
 
 chosen=$(echo -e "$choices" | dmenu -i -p "Select application:")
 
@@ -21,4 +21,12 @@ case "$chosen" in
         exec gnome-terminal -e "vim $HOME/.zshrc" ;;
     i3config )
         exec gnome-terminal -e "vim $HOME/.config/i3/config" ;;
+    xdman )
+        exec xdman ;;
+    qbittorrent )
+        exec qbittorrent ;;
+    cmus )
+        exec gnome-terminal -e cmus ;;
+    cava )
+        exec gnome-terminal -e cava ;;
 esac
