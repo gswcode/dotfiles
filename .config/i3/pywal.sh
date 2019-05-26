67751ef645f6814335b21e3f270b39307844e825
Gaurav Singh <gs1293@gmail.com>
2019-05-26 21:42:49 +0530

    Updating config files

--- .config/i3/pywal.sh
+++ .config/i3/pywal.sh
@@ -2,8 +2,27 @@
 
 # Script to change wallpapers and colorscheme using PYWAL
 
+# Randomly selecting a number between 1 and 6
+random_number=$((1 + RANDOM % 10))
+
 # Setting the wallpaper randomly using PYWAL
-wal --vte -q -i "${HOME}/walls/wallpapers/"
+
+case $random_number in
+    1 )
+        wal --vte -q -i "${HOME}/walls/walls_anime" ;;
+    2 )
+        wal --vte -q -i "${HOME}/walls/walls_cartoon" ;;
+    3 )
+        wal --vte -q -i "${HOME}/walls/walls_linux" ;;
+    4 )
+        wal --vte -q -i "${HOME}/walls/walls_movies" ;;
+    5 )
+        wal --vte -q -i "${HOME}/walls/walls_other" ;;
+    6 )
+        wal --vte -q -i "${HOME}/walls/walls_tv" ;;
+    * )
+        wal --vte -q -i "${HOME}/walls/pywal" ;;
+esac
 
 # Removing any previously used wallpaper
 rm -rf "${HOME}/walls/pywal/"*
